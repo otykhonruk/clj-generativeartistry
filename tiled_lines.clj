7dd9db95ee495afea5ebbd60d16944275be89670
Oleksandr Tykhonruk <a.tihonruk@gmail.com>
2022-01-19 18:09:05 +0300

    Example #1. Tiled lines.

---
+++ tiled_lines.clj
@@ -0,0 +1,44 @@
+(ns generativeartistry.tiled-lines
+  (:require [quil.core :refer :all]))
+
+(defn setup []
+  (no-loop))
+
+
+(defn grid
+  [w h]
+  (for [x (range w)
+        y (range h)]
+    [x y]))
+
+
+(defn draw-cell
+  [x y cellsize]
+  (let [ox (* x cellsize) 
+        oy (* y cellsize)
+        x (+ ox cellsize)
+        y (+ oy cellsize)]
+    (if (zero? (rand-int 2))
+      (line ox oy x y)
+      (line ox y x oy))))
+
+
+(defn draw-grid
+  [grid cellsize]
+  (doseq [[x y] grid]
+    (draw-cell x y cellsize)))
+
+
+(defn draw []
+  (background 248)
+  (stroke-weight 2)
+  (doseq [[x y] (grid 50 50)]
+    (draw-cell x y 15)))
+
+
+(defsketch tiled-lines
+  :title "Tiled lines"
+  :size [750 750]
+  :setup setup
+  :draw draw
+  :features [:keep-on-top])
